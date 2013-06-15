7bb514cc2b11af84556429ab9ef2c6a4f2424df6
clemos <cl3mos@gmail.com>
2013-06-15 12:44:30 +0200

    obvious

--- js/Node.hx
+++ js/Node.hx
@@ -15,9 +15,9 @@ class Node {
 
   public static var console(default,null) : js.node.stdio.Console = untyped __js__('console');
   public static var process(default,null) : js.node.Process = untyped __js__('process');
-  public static var module(default,null) : js.node.Process = untyped __js__('module');
-  public static var __filename(default,null) : js.node.Process = untyped __js__('__filename');
-  public static var __dirname(default,null) : js.node.Process = untyped __js__('__dirname');
+  public static var module(default,null) : Dynamic = untyped __js__('module');
+  public static var __filename(default,null) : String = untyped __js__('__filename');
+  public static var __dirname(default,null) : String = untyped __js__('__dirname');
 
   public static var require(default,null): String->Dynamic = untyped __js__("require");
   public static var setTimeout(default,null): (Void->Void)->Int->?Array<String>->Int = untyped __js__("setTimeout");
