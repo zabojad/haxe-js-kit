7747f110ae16a40049a8102097e871145e1237f1
clemos <cl3mos@gmail.com>
2013-07-28 16:48:56 +0200

    cleaned up regexp support

--- js/support/RegExp.hx
+++ js/support/RegExp.hx
@@ -1,13 +1,14 @@
 package js.support;
 
 abstract RegExp(js.native.RegExp) {
-	inline function new( regexp : js.native.RegExp ){
-		this = regexp;
-	}
 	@:from public inline static function fromEReg( r : EReg ){
-		return new RegExp( RegExpTools.toRegExp( r ) );
+		return untyped r.r;
 	}
 	@:to public inline static function toEReg( r : js.native.RegExp ) : EReg {
-		return RegExpTools.toEReg( r );
+		return new EReg( r.source , 
+			( r.ignoreCase ? "i" : "" ) 
+			+ ( r.global ? "g" : "" ) 
+			+ ( r.multiline ? "m" : "" )  
+		);
 	}
 }
