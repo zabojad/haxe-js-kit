9afd89dda0c51cabdc38a61083c6be59dd62b7b6
clemos <cl3mos@gmail.com>
2013-07-28 17:18:21 +0200

    uses @:native

--- js/npm/connect/Session.hx
+++ js/npm/connect/Session.hx
@@ -5,8 +5,9 @@ import js.npm.Connect;
 import js.npm.connect.session.Cookie;
 import js.npm.connect.session.Store;
 
+@:native('session')
 extern class Session 
-implements npm.Package.Require<"connect","*"> #if !haxe3,#end
+implements npm.Package.RequireNamespace<"connect","*"> #if !haxe3,#end
 implements js.npm.connect.Middleware
 {
 
@@ -20,10 +21,6 @@ implements js.npm.connect.Middleware
 
 	public inline static function session( req : ClientRequest ) : js.npm.connect.session.Session {
 		return untyped req.session;
-	}
-
-	static function __init__():Void{
-		untyped Session = Connect.session;
 	}
 
 }
