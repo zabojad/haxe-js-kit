24e4e39120b85d157bc07dbc538b1ceb75fce6f0
Peekmo <axel.anceau@gmail.com>
2014-02-12 21:43:37 +0100

    Additionals methods on ChildProcess & Cluster + bug fixes on Cluster's events

--- js/node/Cluster.hx
+++ js/node/Cluster.hx
@@ -8,22 +8,44 @@ typedef ClusterSettings = {
 
 
 /* emits: message, online,listening,disconnect,exit, setup */
-extern class ClusterWorker extends js.Node.NodeEventEmitter {
+@:native("Worker")
+extern class ClusterWorker extends js.node.events.EventEmitter {
+    public static inline var EVENT_ONLINE = 'online';
+    public static inline var EVENT_LISTENING =  'listening';
+    public static inline var EVENT_EXIT = 'exit';
+    public static inline var EVENT_DISCONNECT = 'disconnect';
+    public static inline var EVENT_MESSAGE = 'message';
+    public static inline var EVENT_ERROR = 'error';
+
     var uniqueID:String; // indexes into cluster.workers
     var process:js.node.ChildProcess;
     var suicide:Bool;
     function send(message:Dynamic,?sendHandle:Dynamic):Void;
     function destroy():Void;
+    function disconnect(): Void;
 }
 
 /* Emits: death,message, fork, online, listening  */
-extern class Cluster extends js.Node.EventEmitter 
+extern class Cluster extends js.node.events.EventEmitter
 implements npm.Package.Require<"cluster","*"> {
-  static var isMaster:Bool;
-  static var isWorker:Bool;
-  static var workers:Array<ClusterWorker>;
-  static function fork(?env:Dynamic):ClusterWorker;
-  static function send(o:Dynamic):Void;
-  static function setupMaster(?settings:ClusterSettings):Void;
-  static function disconnect(?cb:Void->Void):Void;
+    public static inline var EVENT_FORK = 'fork';
+    public static inline var EVENT_ONLINE = 'online';
+    public static inline var EVENT_LISTENING =  'listening';
+    public static inline var EVENT_EXIT = 'exit';
+    public static inline var EVENT_DISCONNECT = 'disconnect';
+    public static inline var EVENT_SETUP = 'setup';
+
+    static var isMaster:Bool;
+    static var isWorker:Bool;
+    static var worker: ClusterWorker;
+    static var workers:Array<ClusterWorker>;
+    static function fork(?env:Dynamic):ClusterWorker;
+    static function send(o:Dynamic):Void;
+    static function setupMaster(?settings:ClusterSettings):Void;
+    static function disconnect(?cb:Void->Void):Void;
+
+    // Needed to use events on master Cluster
+    public static var _ : Cluster;
+    static function __init__() : Void 
+      _ = untyped Cluster;
 }
