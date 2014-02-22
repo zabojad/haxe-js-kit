19eaa9f53b75da702d0589200263b0e4524c1144
clemos <cl3mos@gmail.com>
2014-02-22 13:17:29 +0100

    git push origin master

--- js/support/RegExp.hx
+++ js/support/RegExp.hx
@@ -1,5 +1,12 @@
 package js.support;
 
+/**
+
+	An abstract that wraps native JS RegExp, and can transtype to haxe EReg
+	It can be used in extern definitions to allow both native Regexp and haxe EReg
+
+**/
+
 abstract RegExp(js.RegExp) {
 	@:from public inline static function fromEReg( r : EReg ){
 		return untyped r.r;
