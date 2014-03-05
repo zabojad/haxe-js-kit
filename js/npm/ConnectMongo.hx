885266d3a559b5d17fe191740484d6e625cd43b8
clemos <cl3mos@gmail.com>
2014-03-05 16:13:10 +0100

    added version to express, as 4.0 is out and breaks things

--- js/npm/ConnectMongo.hx
+++ js/npm/ConnectMongo.hx
@@ -4,7 +4,7 @@ extern class ConnectMongo
 implements npm.Package.Require<"connect-mongo","*"> {
 	public function new( expr : Dynamic ) : Void;
 	public static inline function store( opts : {} ) : js.npm.connect.session.Store {
-		var cm = untyped ConnectMongo(js.npm.Express);
+		var cm = untyped ConnectMongo(js.npm.Connect);
 		return untyped __new__(cm ,opts );
 	}
 }
--- js/npm/Express.hx
+++ js/npm/Express.hx
@@ -2,7 +2,7 @@ package js.npm;
 
 extern class Express 
 extends js.npm.express.Application #if !haxe3,#end 
-implements npm.Package.Require<"express","latest"> {
+implements npm.Package.Require<"express","~3.0"> {
 
 	public function new() : Void;
 	
