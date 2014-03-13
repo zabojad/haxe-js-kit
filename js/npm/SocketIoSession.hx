87fa581c780e30407bb8c9a68e94b7fcb1266264
clemos <cl3mos@gmail.com>
2014-03-13 10:57:05 +0100

    null check for socket.handshake (which seems to be null sometimes...)

--- js/npm/SocketIoSession.hx
+++ js/npm/SocketIoSession.hx
@@ -8,7 +8,7 @@ implements npm.Package.Require<"socket.io-session","*"> {
 	public function new( cookieParser : js.npm.connect.Middleware , store : js.npm.connect.session.Store ) : Void;
 
 	public static inline function session( namespace : Namespace ) : Dynamic {
-		return untyped namespace.handshake.session;
+		return untyped (namespace.handshake == null) ? null : namespace.handshake.session;
 	}
 
 }
