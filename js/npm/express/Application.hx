cdfb4588673e450bdbcbb7b06a9de116c8604e26
clemos <cl3mos@gmail.com>
2014-04-14 09:49:58 +0200

    remove duplicate declaration, return Application

--- js/npm/express/Application.hx
+++ js/npm/express/Application.hx
@@ -11,15 +11,14 @@ extern class Application
 
 	public var locals : Dynamic;
 
-	@:overload( function ( mount : String , middleware : Middleware<Request,Response> ) : Connect {} )
-	@:overload( function ( mount : String , middleware : Middleware<Request,Response> ) : Connect {} )
-	@:overload( function ( errorHandler : Dynamic -> Request -> Response -> Callback0 -> Void  ) : Connect {} )
-	public function use ( middleware : Middleware<Request,Response> ) : Connect ;
+	@:overload( function ( mount : String , middleware : Middleware<Request,Response> ) : Application {} )
+	@:overload( function ( errorHandler : Dynamic -> Request -> Response -> Callback0 -> Void  ) : Application {} )
+	public function use ( middleware : Middleware<Request,Response> ) : Application ;
 
 	@:overload(function( port :Int, ready : Void -> Void ): Application { } )
 	public function listen (port :Int, ?address :String) : Application;
 
-	public static function createServer (a1 :Dynamic, ?a2 :Dynamic, ?a3 :Dynamic, ?a4 :Dynamic, ?a5 :Dynamic, ?a6 :Dynamic, ?a7 :Dynamic, ?a8 :Dynamic, ?a9 :Dynamic) : Connect;
+	public static function createServer (a1 :Dynamic, ?a2 :Dynamic, ?a3 :Dynamic, ?a4 :Dynamic, ?a5 :Dynamic, ?a6 :Dynamic, ?a7 :Dynamic, ?a8 :Dynamic, ?a9 :Dynamic) : Application;
 
 	@:overload(function(path : RegExp , f1 : Middleware<Request,Response>, f2 : Middleware<Request,Response> ) : Void {} )
 	@:overload(function(path : String , f1 : Middleware<Request,Response>, f2 : Middleware<Request,Response> ) : Void {} )
