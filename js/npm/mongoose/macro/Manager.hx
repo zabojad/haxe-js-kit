c76c03133acaccdc164afacf463c24be3f1a855e
clemos <cl3mos@gmail.com>
2014-04-16 11:27:34 +0200

    unneeded

--- js/npm/mongoose/macro/Manager.hx
+++ js/npm/mongoose/macro/Manager.hx
@@ -6,6 +6,4 @@ import js.npm.mongoose.Mongoose;
 @:autoBuild( util.Mongoose.buildManager( js.npm.mongoose.macro.Manager ) )
 class Manager<T,M>
 extends js.npm.mongoose.Model.TModels<T,M>
-{
-	public function new(){}
-}
+{}
