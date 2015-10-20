4cdeaed465a519f99296a9a7dd6ff8ad4e16e3ec
kevin kf <kevinkf@awesapp.com>
2015-10-20 12:36:06 +0800

    Fix Type

--- js/npm/mongoose/Model.hx
+++ js/npm/mongoose/Model.hx
@@ -64,9 +64,9 @@ extern class TModels<T,M:TModel<T>> {
 	public function find( ?conditions : {} , ?callback : Callback<Array<M>> ): Query<Array<M>>; // Query<Model<T>>
 
 	@:overload( function( id : Dynamic , callback : Callback<Null<M>> ) : Void {} )
-	@:overload( function ( id : Dynamic , fields : String , options : {} , ?callback : Callback<Null<Model<T>>> ): Query<Model<T>> {} )
-	@:overload( function ( id : Dynamic , fields : Null<{}> , options : {} , ?callback : Callback<Null<Model<T>>> ): Query<Model<T>> {} )
-	public function findById( id : Dynamic , ?callback : Callback<Null<Model<T>>> ): Query<Model<T>>; // Query<Model<T>>
+	@:overload( function ( id : Dynamic , fields : String , options : {} , ?callback : Callback<Null<M>> ): Query<Model<T>> {} )
+	@:overload( function ( id : Dynamic , fields : Null<{}> , options : {} , ?callback : Callback<Null<M>> ): Query<Model<T>> {} )
+	public function findById( id : Dynamic ): Query<Model<T>>; // Query<Model<T>>
 
 	@:overload( function( conditions : {} , callback : Callback<Null<M>> ) : Void {} )
 	@:overload( function ( conditions : {}  , fields : String , options : {} , ?callback : Callback<Null<M>> ): Query<M> {} )
