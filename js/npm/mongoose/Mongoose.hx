ec8120c324609b1c67569713bb0c1c3f148131bf
Kevin Leung <kevinresol@users.noreply.github.com>
2015-10-23 09:24:41 +0800

    Add method overload for connect()

--- js/npm/mongoose/Mongoose.hx
+++ js/npm/mongoose/Mongoose.hx
@@ -13,6 +13,8 @@ implements npm.Package.RequireNamespace<"mongoose","^4.0.0"> {
 
 	public function disconnect(cb : Callback0) : Void;
 	public function createConnection( url : String ) : Connection;
+	
+	@:overload(function( url : String , options:{}, ?fn : js.support.Callback0 ) : Mongoose {})
 	public function connect( url : String , ?fn : js.support.Callback0 ) : Mongoose;
 	public function model<T,M:Model.TModel<T>>( name : String , ?schema : Schema<T> , ?collectionName : String , ?skipInit : Bool ) : Model.TModels<T,M>;
 	public function plugin( fn : Dynamic , ?options : {} ) : Mongoose;
