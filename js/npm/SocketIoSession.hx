3a2c54c7ca0eae21bd925ebbeb37822db1708ff3
clemos <cl3mos@gmail.com>
2015-12-18 12:19:47 +0100

    remove entirely Connect.

--- js/npm/SocketIoSession.hx
+++ js/npm/SocketIoSession.hx
@@ -5,7 +5,7 @@ import js.npm.socketio.Namespace;
 extern class SocketIoSession
 implements npm.Package.Require<"socket.io-session","*"> {
 
-	public function new( cookieParser : js.npm.connect.Middleware<Dynamic,Dynamic> , store : js.npm.connect.session.Store ) : Void;
+	public function new( cookieParser : js.npm.express.Middleware , store : js.npm.express.session.Store ) : Void;
 
 	public static inline function session( namespace : Namespace ) : Dynamic {
 		return untyped (namespace.handshake == null) ? null : namespace.handshake.session;
