1e85e07994a142db2159d0aa2ec59add0a0968ef
Thomas FETIVEAU <thomas.fetiveau.tech@gmail.com>
2016-03-07 14:50:44 +0100

    Add basic externs for redux and redux-thunk

---
+++ js/npm/Redux.hx
@@ -0,0 +1,10 @@
+package js.npm;
+
+/**
+Extern for [Redux](https://github.com/reactjs/redux)
+**/
+@:jsRequire('redux')
+extern class Redux {
+	static public function createStore(reducer : Dynamic, ? initialState : Dynamic, ? enhancer : Dynamic) : Dynamic;
+	static public function applyMiddleware(mdlw : haxe.extern.Rest<Dynamic>) : Dynamic;
+}
---
+++ js/npm/ReduxThunk.hx
@@ -0,0 +1,7 @@
+package js.npm;
+
+/**
+Extern for [ReduxThunk](https://github.com/gaearon/redux-thunk)
+**/
+@:jsRequire('redux-thunk')
+extern class ReduxThunk {}
