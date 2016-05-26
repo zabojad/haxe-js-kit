c1e4a180bd37a1c10de69820290b00716e658933
Thomas FETIVEAU <thomas.fetiveau.tech@gmail.com>
2016-05-26 10:06:34 +0200

    Fix ReduxThunk

--- js/npm/ReduxThunk.hx
+++ js/npm/ReduxThunk.hx
@@ -3,5 +3,5 @@ package js.npm;
 /**
 Extern for [ReduxThunk](https://github.com/gaearon/redux-thunk)
 **/
-@:jsRequire('redux-thunk')
+@:jsRequire('redux-thunk','default')
 extern class ReduxThunk {}
